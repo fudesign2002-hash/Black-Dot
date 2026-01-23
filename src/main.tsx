1b118fcc7b87589a0225efb9a017d398ab14ebc3
fudesign2002-hash <fudesign2002@gmail.com>
2026-01-23 08:42:56 -0500

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './input.css';
+import './Input.css';
 import App from '../App'; // 注意：你的 App.tsx 在專案根目錄，不在 src 裡
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
