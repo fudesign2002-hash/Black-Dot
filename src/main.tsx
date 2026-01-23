9e65b2f029cf10a60c470b6d6908b4b8598ab181
fudesign2002-hash <fudesign2002@gmail.com>
2026-01-23 08:45:27 -0500

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './input.css';
 import App from '../App'; // 注意：你的 App.tsx 在專案根目錄，不在 src 裡
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
